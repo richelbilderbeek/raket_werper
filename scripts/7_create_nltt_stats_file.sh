bc50923f5dba930471b488e438b5aebf9b915981
richelbilderbeek <richel@richelbilderbeek.nl>
2019-06-24 11:41:04 +0200

    Copy script files from razzo [skip ci]

---
+++ scripts/7_create_nltt_stats_file.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Combine all generated nLTT files to one nLTT file in results
+#
+# Usage, locally:
+#
+#   ./scripts/7_create_nltt_stats_file
+#
+# Usage, on Peregrine:
+#
+#   sbatch ./scripts/7_create_nltt_stats_file
+#
+# Peregrine directives:
+#SBATCH --time=1:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=1G
+#SBATCH --job-name=7_create_nltt_stats_file
+#SBATCH --output=7_create_nltt_stats_file.log
+module load GCCcore/4.9.3 
+module load XZ/5.2.2-foss-2016a
+module load R
+Rscript -e 'razzo::create_nltt_stats_file()'
